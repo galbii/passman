aafa2a859040104a0b53f8e2c0059e38ddf577b5
galbii <galbii@protonmail.com>
2022-10-30 12:03:24 -0700

    added functionality for the main file yay,q the first working version of passman.py errors with adding multiple of same entries

--- main.py
+++ main.py
@@ -3,10 +3,29 @@ from passman import PassMan as pm
 """Objective No.1: create a function to take input of a master password and hash it to store it into a json file"""
     
 key_path = 'public_key'
+def print_menu():
+    choice = input("Please select an option\n\t\t1. Display website entry information\n\t\t2. Add an entry to the file.\n\t\t3. Remove and entry from the file.\n\t\t4. Clear an entry from the file\n\t\t5. Quit\n>")
+    return choice
 
 if __name__ == "__main__":
     pm = pm()
-    pm.add_entry('', 'hh', 'random password')
-    pm.del_entry('tithub')
-    print(pm.passtable)
-
+    print("Welcome to passman, a password manager that stores your data in a pandas dataframe in a file.\nYour default file will be stored in 'passwords' and you can edit them in the pasman.py file in the initializing funtion\n") 
+    choice = 0
+    while choice != 5:
+        choice = int(print_menu())
+        print(choice)
+        if choice == 1:
+            website = input("Please enter the website that you would like to retrieve:\n>")
+            pm.get_entry(website)
+        elif choice == 2:
+            website = input("Website:\n")
+            login = input("Login:\n")
+            password = input("Password:\n")
+            pm.add_entry(website, login, password)
+        elif choice == 3:
+            website = input("Please enter the website that you would like to delete:\n>")
+            pm.del_entry(website)
+        elif choice == 4:
+            print('remove')
+        elif choice == 5:
+            pm.save_db()
--- passman.py
+++ passman.py
@@ -12,15 +12,22 @@ class PassMan():
         self.path = "public_key"
         self.key = None
 
-    def save_db(self, df):
-        df.to_csv(self.password_file)
-        print('success')
+    def save_db(self):
+        self.passtable.to_csv(self.password_file)
+        print('[success] thank you for using passman')
     
     #def inc_dig(dig):
     """edit this function to manually check if an entry is correct see test.py to see the test results i.e. df.loc['ya', 'hii'] == entry['hii'] food for thought, assign df.loc[certain_entry] to a variable and then do comparisons with single row values"""
+    def get_entry(self, website):
+        try:
+            print(self.passtable.loc[website])
+        except:
+            print("[error] entry not found")
+
     def check_entry(self, web, entry, mode : Optional[str]="unmatch"):
         if web in self.passtable.index:
             print('bitch')
+            """This if statement check to see if the entry login and password matches"""
             if self.passtable.loc[web]["Login"] == entry[0] and self.passtable.loc[web]["Password"] == entry[1] and mode == "match":
                 return True
             elif mode == "unmatch":
@@ -33,10 +40,10 @@ class PassMan():
             return False
         
         """gonna make it so that only one single instance of the website will
-        be accomodated for"""
+        be accomodated for
+        edit: lol good luck figuring out what this function does xdxdxdxd"""
     def add_entry(self, website, login, password):
         entry = [login, password]
-        df = self.passtable
         #print(df.loc[website][["Login","Password"]] )
         #df.loc[website] = [login, password]
         if self.check_entry(website, entry):
@@ -44,27 +51,23 @@ class PassMan():
                 print('Entry is already added')
             elif not self.check_entry(website+"1", entry, mode = "match"):
                 """ovverride needed here define override function tomorrow"""
-                df.loc[website + str(int(website[-1])+1)] = entry
+                self.passtable.loc[website + str(int(website[-1])+1)] = entry
                 print("you're gay")
             else:
-                df.loc[website+'1'] = entry
+                self.passtable.loc[website+'1'] = entry
         else:
-            df.loc[website] = entry
+            self.passtable.loc[website] = entry
 #        try:
-#            if df.loc[website] == [login, password]:
-#                df.loc[website+'1'] = [login, password]
+#            if self.passtable.loc[website] == [login, password]:
+#                self.passtable.loc[website+'1'] = [login, password]
 #            else:
 #                df.loc[website] = [login, password]
 #        except:
 #            df.loc[website] = [login, password]
-        self.passtable = df
-        self.save_db(df)
 
     def del_entry(self, website):
        self.passtable.drop(website, axis = "index", inplace = True) 
-       self.save_db(self.passtable)
        print("success")
 
     """workign on a clear table entry to delete the entire dataframe"""
-    def clear_table(self):
-        
+#    def clear_table(self):
